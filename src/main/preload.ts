f68590f748751ef71de269ceb53683f87e4c2fe8
Bob Vandevliet <info@bvandevliet.nl>
2022-01-21 16:33:25 +0100

    fixed width explorer sidebar, wip though

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -7,25 +7,6 @@ import
   from 'electron';
 
 /**
- * Print version information to the browser window.
- *
- * @link https://www.electronjs.org/docs/latest/tutorial/quick-start
- */
-window.addEventListener('DOMContentLoaded', () =>
-{
-  const replaceText = (selector: string, text: string) =>
-  {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  for (const dependency of ['chrome', 'node', 'electron'])
-  {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
-});
-
-/**
  * The API to expose to the global Window object
  */
 export const API =
