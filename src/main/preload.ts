7b4532ace6a163e3e2024026797b780fcdb7de9d
Bob Vandevliet <info@bvandevliet.nl>
2022-01-24 08:35:32 +0100

    added json config file handler

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,8 +1,9 @@
-// import * as path from 'path';
-// import * as fs from 'fs';
+import * as path from 'path';
+import * as fs from 'fs';
+// import { exec } from 'child_process';
 import
 {
-  contextBridge, ipcRenderer,
+  app, contextBridge, ipcRenderer,
 }
   from 'electron';
 
@@ -30,6 +31,98 @@ window.addEventListener('DOMContentLoaded', () =>
  */
 export const API =
 {
+  /**
+   * Merge user defined values with defaults.
+   *
+   * @param input    Arguments to merge with default values.
+   * @param defaults Object that serves as the defaults.
+   */
+  parseArgs: (input: Record<any, unknown>, defaults: Record<any, unknown> = {}) =>
+  {
+    Object.keys(input).forEach(key => defaults[key] = input[key]);
+
+    return defaults;
+  },
+
+  /**
+   * Parse a json file.
+   *
+   * @param filePath Path to the json config file.
+   * @param defaults Object that serves as the defaults.
+   */
+  parseConfigFile: (filePath: fs.PathLike, defaults: Record<any, unknown> = {}): Record<any, unknown> =>
+  {
+    try
+    {
+      return API.parseArgs(JSON.parse(fs.readFileSync(filePath).toString()), defaults);
+    }
+    catch (err)
+    {
+      return defaults;
+    }
+  },
+
+  /**
+   * Configuration file handler.
+   */
+  configFile: class
+  {
+    private _path: fs.PathLike;
+    private _data: Record<any, unknown>;
+
+    /**
+     * Path to the json config file.
+     */
+    public get path () { return this._path; }
+
+    /**
+     * The json config data.
+     */
+    public get data () { return this._data; }
+
+    /**
+     *
+     * @param configName Name of the json config file.
+     * @param defaults   Object that serves as the defaults in case the file can't be found or parsed.
+     */
+    constructor (configName = 'config', defaults: Record<any, unknown> = {})
+    {
+      this._path = path.join(app.getPath('userData'), `${configName}.json`);
+      this._data = API.parseConfigFile(this._path, defaults);
+    }
+
+    /**
+     * Returns the property value on the `data` object.
+     *
+     * @param key Property name.
+     */
+    get (key: any)
+    {
+      return this._data[key];
+    }
+
+    /**
+     * Set the property value on the `data` object.
+     *
+     * @param key Property name.
+     */
+    set (key: any, val: unknown)
+    {
+      this._data[key] = val;
+      this.write();
+    }
+
+    /**
+     * Writes the current object state to the linked file.
+     */
+    write ()
+    {
+      // If we had used an async API and our app was quit before the
+      // async write had a chance to complete, we might lose that data.
+      fs.writeFileSync(this._path, JSON.stringify(this._data));
+    }
+  },
+
   /**
    * Start Native File Drag & Drop.
    *
