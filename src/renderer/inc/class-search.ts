df793051235941a257f496ee18f466d748f3c144
Bob Vandevliet <info@bvandevliet.nl>
2022-01-25 11:54:14 +0100

    added elem sort function and Search class

---
+++ src/renderer/inc/class-search.ts
@@ -0,0 +1,142 @@
+/**
+ * Class for filtering elements by a search query.
+ */
+export default class Search
+{
+  /**
+   * The container element that is a close parent of the `elemSelect` selector.
+   */
+  public $elemMaster: JQuery<HTMLElement>;
+
+  /**
+   * The selector string that refers to the elements to show/hide relative to `elemMaster`.
+   */
+  public elemSelector: string;
+
+  /**
+   * The callback function to return a value to search in.
+   */
+  public searchIn: (curElem: HTMLElement) => string|string[];
+
+  /**
+   * Internal search timeout timer.
+   */
+  private _timer: NodeJS.Timeout;
+
+  /**
+   *
+   * @param elemMaster   A container element that is a close parent of the `elemSelect` selector.
+   * @param elemSelector A selector string that refers to the elements to show/hide relative to `elemMaster`.
+   * @param searchIn     A callback function to return a value to search in.
+   */
+  constructor
+  (
+    elemMaster: any,
+    elemSelector: string,
+    searchIn: (curElem: HTMLElement) => string|string[],
+  )
+  {
+    this.$elemMaster = $(elemMaster);
+    this.elemSelector = elemSelector;
+    this.searchIn = searchIn;
+  }
+
+  /**
+   * Split a string or string array by whitespaces and flatten it.
+   *
+   * @param input String or string array to split and flatten.
+   */
+  static flatten (input: string|string[])
+  {
+    return (!Array.isArray(input) ? [input] : input).flatMap(str => str.replace(/\s+/gu, ' ').trim().split(' '));
+  }
+
+  /**
+   * Test if a string contains a substring, ignoring diacritics.
+   *
+   * @param haystack
+   * @param needle
+   */
+  static includes (haystack: string, needle: string)
+  {
+    const decomposedA = haystack.normalize('NFD').replace(/[\u0300-\u036f]/gu, '').toLowerCase();
+    const decomposedB = needle.normalize('NFD').replace(/[\u0300-\u036f]/gu, '').toLowerCase();
+
+    return decomposedA.includes(decomposedB);
+  }
+
+  /**
+   * Test if a search array matches a search query.
+   *
+   * @param searchIn  String or array to search in.
+   * @param searchFor String or array of search queries.
+   *                  These can contain negative queries starting with `!`
+   *                  and (negative) regex queries starting with `(!)^`.
+   */
+  static isMatch (searchIn: string|string[], searchFor: string|string[])
+  {
+    searchIn = Search.flatten(searchIn);
+    searchFor = Search.flatten(searchFor);
+
+    return searchFor.every(queryItem =>
+    {
+      const negative = queryItem.startsWith('!');
+
+      if (negative)
+      {
+        queryItem = queryItem.substring(1);
+      }
+
+      let queryRegex: RegExp = null;
+
+      if (queryItem.startsWith('^'))
+      {
+        try
+        {
+          queryRegex = new RegExp(queryItem.substring(1), 'iu');
+        }
+        catch (err)
+        {
+          return false;
+        }
+      }
+
+      // Does every positive query item match at least one search item?
+      // And does every negative query segment not match any search segment?
+      return negative !== (searchIn as string[]).some(searchItem =>
+        queryRegex ? queryRegex.test(searchItem) : Search.includes(queryItem, searchItem));
+    });
+  }
+
+  /**
+   * Perform a search action on this instance.
+   *
+   * @param queryStr The search query string.
+   */
+  search (queryStr: string)
+  {
+    clearTimeout(this._timer);
+    this._timer = setTimeout(() =>
+    {
+      const $elemSet = this.$elemMaster.find(this.elemSelector);
+
+      if (!queryStr.trim().length)
+      {
+        $elemSet.show(); return;
+      }
+
+      // Loop through each parent element and show/hide it based on search query matching.
+      $elemSet.each((_i, elemParent) =>
+      {
+        const $elemParent = $(elemParent);
+
+        if ($elemParent.hasClass('ignore')) return;
+
+        const searchArr = this.searchIn(elemParent);
+
+        Search.isMatch(searchArr, queryStr) ? $elemParent.show() : $elemParent.hide();
+      });
+    },
+    200);
+  }
+}
--- src/renderer/inc/functions-html.ts
+++ src/renderer/inc/functions-html.ts
@@ -1,3 +1,37 @@
+// eslint-disable-next-line no-shadow
+export enum Order
+{
+  ASC,
+  DESC,
+}
+
+/**
+ * Sort elements alphabetically and numerically.
+ *
+ * @param elemSet The set of elements to sort.
+ * @param sortBy  Callback function to return a value to sort by.
+ * @param orderBy Sort order. Default is `ASC`.
+ */
+export const sortElement =
+(
+  elemSet: any,
+  sortBy: (curElem: HTMLElement) => string|number,
+  orderBy: Order = Order.ASC,
+) =>
+{
+  const doSort = (a: HTMLElement, b: HTMLElement) =>
+  {
+    const textA = sortBy(a).toString().trim();
+    const textB = sortBy(b).toString().trim();
+
+    return textA.localeCompare(textB, undefined, { numeric: true, sensitivity: 'base' });
+  };
+
+  $(elemSet).get()
+    .sort((a, b) => orderBy === Order.DESC ? doSort(b, a) : doSort(a, b))
+    .forEach(curElem => $(curElem).parent().append(curElem));
+};
+
 /**
  * Copy a string to the clipboard.
  *
