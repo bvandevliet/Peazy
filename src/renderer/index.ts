bbdab8b5cd8c6638b547da22930637eeb290dfe5
Bob Vandevliet <info@bvandevliet.nl>
2022-01-26 09:55:15 +0100

    added browsing state toggle, splitting scss files

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -10,6 +10,40 @@ document.addEventListener('copy', e =>
 });
 
 /**
+ * Toggle browsing state on key input.
+ */
+document.addEventListener('keyup', e =>
+{
+  if (e.ctrlKey && e.shiftKey && e.key === 'F')
+  {
+    $(document.body).addClass('browsing');
+    // setTimeout(() => { $project_search[0].focus(); });
+  }
+  else if (e.key === 'Escape')
+  {
+    $(document.body).removeClass('browsing');
+    // setTimeout(() => { $project_search[0].blur(); });
+  }
+});
+
+/**
+ * Toggle browsing state on sidebar mouse input.
+ */
+$('#sidebar').on('mouseup', function (e)
+{
+  // Remove browsing state for links that open a main tab.
+  if ($(e.target).is('a.opens-main-tab'))
+  {
+    $(document.body).removeClass('browsing');
+  }
+  // Taking care of scrollbar.
+  else if (e.clientX < this.clientWidth - 18)
+  {
+    $(document.body).addClass('browsing');
+  }
+});
+
+/**
  * A filter example.
  */
 window.addEventListener('DOMContentLoaded', () =>
