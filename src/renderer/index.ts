36a80d8ec9c5c7f527a2a231a8da49e8cfac28dc
Bob Vandevliet <info@bvandevliet.nl>
2022-01-27 09:48:02 +0100

    prevent drag dropping and opening of external files

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,13 +1,16 @@
 // import * as html from './inc/functions-html.js';
 
 /**
- * Make sure to always copy plain text without styling.
+ * Prevent default 'dragover' and 'drop' behaviour.
  */
+document.addEventListener('dragover drop', e =>
+  (e.preventDefault(), e.stopPropagation(), false));
+
+/**
+* Make sure to always copy plain text without styling.
+*/
 document.addEventListener('copy', e =>
-{
-  e.preventDefault();
-  e.clipboardData.setData('text/plain', document.getSelection().toString());
-});
+  (e.preventDefault(), e.clipboardData.setData('text/plain', document.getSelection().toString())));
 
 /**
  * A filter example.
