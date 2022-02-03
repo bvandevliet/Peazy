d2564d0ca574bf3d729def48e7d5b32abbe3e0e3
Bob Vandevliet <info@bvandevliet.nl>
2022-02-03 14:43:48 +0100

    added class Project and associated types

--- src/main/_config/index-example.ts
+++ src/main/_config/index-example.ts
@@ -1,6 +1,9 @@
 /*
- * This module runs in the MAIN process and should only contain the application-wide configuration object.
+ * This module runs in the MAIN process and should at least contain the application-wide configuration object.
  * Just rename this file to `index.ts` and you're good to go.
+ *
+ * You may include custom logic that needs to run in the MAIN process, e.g. additional database related logic.
+ * Using hooks it is possible to expose parts of it to your RENDERER logic.
  */
 
 /**
