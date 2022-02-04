a7d645198668269011d76fbb5e9783d39cf1bef8
Bob Vandevliet <info@bvandevliet.nl>
2022-02-04 09:51:09 +0100

    this hook doesn't exist anymore

--- src/main/_config/index-example.ts
+++ src/main/_config/index-example.ts
@@ -39,14 +39,6 @@ core.addFilter('sql_get_projects', (query: string, args: getProjectArgs) =>
 });
 
 /**
- * SQL query to fetch one project from the database.
- */
-core.addFilter('sql_get_project', (query: string, args: ProjectId) =>
-{
-  return query;
-});
-
-/**
  * SQL query to fetch documents that are attached to a project.
  */
 core.addFilter('sql_get_attached_documents', (query: string, project: Project) =>
