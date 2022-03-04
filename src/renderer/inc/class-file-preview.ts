08bba7cba52b4e1d39e8cb34a1abdc220217c73e
Bob Vandevliet <info@bvandevliet.nl>
2022-03-04 10:18:43 +0100

    first steps adding class FilePreview

---
+++ src/renderer/inc/class-file-preview.ts
@@ -0,0 +1,192 @@
+import Tabs from './class-tabs.js';
+import * as html from './functions-html.js';
+
+/**
+ * A wrapper class to handle html for table lists.
+ */
+export default class FilePreview
+{
+  private _file: string = null;
+
+  /**
+   * The File Preview element.
+   * This is the element you want to add to the DOM.
+   */
+  readonly $preview: JQuery<HTMLDivElement>;
+
+  /**
+   * File Preview uses a `Tabs` instance on the background.
+   */
+  private readonly _preview: Tabs;
+
+  /**
+   * The actual file preview content.
+   */
+  private readonly _$previewContent = $(html.getTemplateClone('tmpl-entry-content')) as JQuery<HTMLDivElement>;
+
+  /**
+   * The file being previewed.
+   */
+  get file ()
+  {
+    return this._file;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private _appendContent = () => {};
+
+  /**
+   * Escape a string to use in a URI query argument.
+   *
+   * @param input The string.
+   */
+  escapeURIComponent = (input: string) =>
+  {
+    return encodeURIComponent(
+      input
+        .replace(/\\/gu, '/')
+        .replace(/%/gu, '%25')
+        // .replace(/\//g, '%2F')
+        // .replace(/ /g, '%20')
+        .replace(/\?/gu, '%3F')
+        .replace(/:/gu, '%3A')
+        .replace(/@/gu, '%40')
+        .replace(/&/gu, '%26')
+        .replace(/=/gu, '%3D')
+        .replace(/\+/gu, '%2B')
+        .replace(/\$/gu, '%24')
+        .replace(/#/gu, '%23'),
+    );
+  };
+
+  private empty ()
+  {
+    this._preview.$ul.find('>li>a').first().text('');
+    this._$previewContent.empty();
+
+    // Collect garbage.
+    window.gc();
+    window.api.gc();
+  }
+
+  /**
+   * Refresh the current preview.
+   */
+  refresh (): boolean
+  {
+    this.empty();
+
+    if (window.api.fs.existsSync(this._file))
+    {
+      this._appendContent();
+
+      return true;
+    }
+    else
+    {
+      // messageBox !!
+
+      return false;
+    }
+  }
+
+  /**
+   * Preview a given file.
+   *
+   * @param file Full path to the file.
+   */
+  preview (file: string)
+  {
+    this._file = window.api.path.normalize(file);
+
+    this.empty();
+
+    const ext = window.api.path.extname(this._file);
+
+    if (/\.(jpe?g|png|gif|bmp)$/iu.test(ext))
+    {
+      this._appendContent = () =>
+      {
+        this._$previewContent.append($(document.createElement('img'))
+          .attr('src', window.api.path.join('file:///', this._file)));
+      };
+    }
+    else
+    {
+      switch (ext)
+      {
+        case 'txt':
+        {
+          this._appendContent = () => null;
+
+          break;
+        }
+        case 'pdf':
+        {
+          const pdfjsVersion = ''; // !!
+
+          const pdfjs = window.api.path.join(window.api.ABSPATH, `ext_modules/pdfjs-${pdfjsVersion}-dist/web/viewer.html`);
+
+          const uriComponent = this.escapeURIComponent(this.file);
+
+          this._appendContent = () =>
+          {
+            this._$previewContent.append($(document.createElement('iframe'))
+              .attr('src', `${window.api.path.join('file:///', pdfjs)}?file=file:///${uriComponent}`));
+          };
+
+          break;
+        }
+        default:
+        {
+          // custom filter !!
+          this._appendContent = () => null;
+        }
+      }
+    }
+
+    return this.refresh();
+  }
+
+  /**
+   *
+   * @param onClose Called when closing the preview.
+   */
+  constructor (onClose?: () => void)
+  {
+    this._preview = new Tabs();
+    this.$preview = this._preview.$container;
+
+    // Create new tab.
+    const { $li } = this._preview.addTab({
+      id: null,
+      template: 'tmpl-li-file-preview',
+      title: 'Click to open file',
+      callback: $div => $div.append(this._$previewContent),
+      onclick: () =>
+      {
+        // Never activate the tab, only perform an action.
+        return new Promise(resolve => (window.api.fs.openNative(this._file), resolve(false)));
+      },
+      onmiddleclick: $li => $li.find('>a.close-btn').first().trigger('click'),
+    },
+    // Don't activate the tab.
+    false);
+
+    // Configure the "refresh" button.
+    $li.find('>a.refresh-btn').on('click', () =>
+    {
+      this.refresh();
+    });
+
+    // Configure the "close" button.
+    $li.find('>a.close-btn').on('click', () =>
+    {
+      this._file = null;
+
+      this.empty();
+
+      if (typeof onClose === 'function') onClose();
+    });
+  }
+}
