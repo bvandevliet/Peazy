bvandevliet/Peazy

commit 999afdfbec20360fb8e4d8ccad4ef07f16decd61

toggle file preview instead of inf refresh when clicking file row

src/renderer/inc/class-html-table-list.ts
@@ -71,11 +71,16 @@ export default class TableList
 
       let $a: JQuery<HTMLElement>;
 
-      const activateRow = () =>
+      const deactivateAll = () =>
       {
         const $table = $tr.parents('table').first();
 
         $table.find('>tbody>tr').removeClass('is-selected');
+      };
+
+      const activateRow = () =>
+      {
+        deactivateAll();
 
         $tr.addClass('is-selected');
       };
@@ -90,7 +95,7 @@ export default class TableList
           {
             e.preventDefault();
 
-            column.onclick($td, $tr, e).then(activate => activate ? (activateRow(), true) : false);
+            column.onclick($td, $tr, e).then(activate => (activate ? activateRow() : deactivateAll(), activate));
           });
       }
       else

src/renderer/tabs/project/files.ts
@@ -120,15 +120,29 @@ export default class filesTab implements tabPage
       {
         text: basename,
         title: basename,
-        onclick: () =>
+        onclick: async (_$td, $tr) =>
         {
-          this.$div.find('.column.sidebar').removeClass('full-width');
-
           // html.loading();
-          return this._filePreview.preview(fileInfo.fullPath)
-            // Always activate.
-            .then(() => true);
-          // .finally(() => html.loading(false));
+
+          const activated = $tr.hasClass('is-selected');
+
+          if (!activated)
+          {
+            // Open File Preview.
+            await this._filePreview.preview(fileInfo.fullPath);
+            this.$div.find('.column.sidebar').removeClass('full-width');
+          }
+          else
+          {
+            // Close File Preview.
+            this._filePreview.close();
+            this.$div.find('.column.sidebar').addClass('full-width');
+          }
+
+          // html.loading(false);
+
+          // Toggle state.
+          return !activated;
         },
         oncontextmenu: () => window.api.hooks.applyFilters('contextmenu_file',
           [

commit 8b120160f327a02b51572c299b63d4a2489f16c0

fixed active project row bug introduced in 999afdfb

src/renderer/inc/class-html-table-list.ts
@@ -95,7 +95,21 @@ export default class TableList
           {
             e.preventDefault();
 
-            column.onclick($td, $tr, e).then(activate => (activate ? activateRow() : deactivateAll(), activate));
+            column.onclick($td, $tr, e).then(activate =>
+            {
+              if (activate)
+              {
+                activateRow();
+
+                return true;
+              }
+              else if (activate === false)
+              {
+                deactivateAll();
+              }
+
+              return false;
+            });
           });
       }
       else

src/renderer/index.ts
@@ -253,7 +253,7 @@ const projectRow = (project: Project) =>
       {
         html.loading();
         return loadProject({ project_number: window.api.hooks.applyFilters('project_project_number', install_number) })
-          .then(found => found ? (stopBrowsing(), false) : false) // if found, still return false /  don't activate row since install number was clicked
+          .then(found => found ? (stopBrowsing(), null) : null) // if found, still return `null` / don't (de)activate any rows / let the project tab handle this
           .finally(() => html.loading(false));
       },
       oncontextmenu: () =>
@@ -274,7 +274,7 @@ const projectRow = (project: Project) =>
       {
         html.loading();
         return loadProject(project)
-          .then(found => found ? (stopBrowsing(), /* true*/ false) : false) // if found, still return false / don't activate row since the tab will handle this
+          .then(found => found ? (stopBrowsing(), /* true*/ null) : null) // if found, still return `null` / don't (de)activate any rows / let the project tab handle this
           .finally(() => html.loading(false));
       },
       oncontextmenu: () =>